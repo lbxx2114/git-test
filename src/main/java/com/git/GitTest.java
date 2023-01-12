96db2c60eb7bd26b0d5288ddb0cc64cfd3d9fa54
xiao <xiao@qq.com>
2023-01-12 16:03:02 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author xiao
+ * 注意：文件的颜色丝毫不会影响代码的执行
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
